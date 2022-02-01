dfa62b57e5cb4d266838510772012db2cd41f430
lrjluiza <lrjluiza@gmail.com>
2022-02-01 14:50:09 -0300

    aprendendo  a usar classes

---
+++ entities/Employee.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Employee {
+
+	public String name;
+	public double grossSalary;
+	public double Tax;
+	
+	public double nextSalary () {
+		return grossSalary - Tax;
+	}
+	
+	public void increaseSalary(double percentage) {
+		grossSalary += grossSalary * percentage / 100;
+	}
+	
+	public String toString() {
+		return name +  ", $ " + String.format("%.2f", nextSalary());
+	}
+
+	
+}
